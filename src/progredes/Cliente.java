6f8e9a37e6154609bcef7ffaef993eb4d601c782
Sandra Wiederkehr <69729146+wiederkehrch@users.noreply.github.com>
2021-06-11 20:41:23 -0300

    Aula Socket

---
+++ src/progredes/Cliente.java
@@ -0,0 +1,31 @@
+package progredes;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+
+	public static void main(String[] args) throws IOException {
+		
+		Socket client = new Socket("127.0.0.1" , 10000);
+		System.out.println ("Cliente conectado ao servidor");
+		
+		//Capturar a mensagem do teclado
+		Scanner s = new Scanner(System.in);
+		PrintStream out = new PrintStream (client.getOutputStream());
+		
+				while(s.hasNextLine()) {
+					
+					out.println (s.nextLine());
+					
+				}
+				
+		out.close();
+		s.close();
+		client.close();
+
+	}
+
+}
